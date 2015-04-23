942da57d5a003230f50d0a8c7f031043ce812669
rcharron <rcphael.charrondiere@ens-lyon.fr>
2015-04-23 19:27:54 +0200

    Settup project with cmake

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+///////////////////////////////////////////////////////////////////////////////
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <DGtal/base/Common.h>
+#include <DGtal/io/boards/Board2D.h>
+#include <DGtal/helpers/StdDefs.h>
+//#include <DGtal/io/readers/PGMReader.h>
+//#include "DGtal/images/imagesSetsUtils/SetFromImage.h"
+#include "DGtal/images/ImageContainerBySTLVector.h"
+///////////////////////////////////////////////////////////////////////////////
+
+using namespace std;
+using namespace DGtal;
+using namespace DGtal::Z2i; //We'll only consider Z² digital space on
+			    //32bit integers
+int main(int argc,char** argv)
+{
+  return 0;
+}
