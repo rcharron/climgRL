c7a8d8eb52f810539dad34d813cc94e69b6e8640
laureline@laureline <laureline.pinault@ens-lyon.fr>
2015-04-24 18:45:58 +0200

    header du traitement antibruit

--- src/Modifieurs/Traitement_AntiBruit.cpp
+++ src/Modifieurs/Traitement_AntiBruit.cpp
@@ -1,16 +1,4 @@
-///////////////////////////////////////////////////////////////////////////////
-#include <iostream>
-#include <vector>
-#include <DGtal/base/Common.h>
-#include <DGtal/io/boards/Board2D.h>
-#include <DGtal/helpers/StdDefs.h>
-#include <DGtal/geometry/curves/ArithmeticalDSSComputer.h>
-#include <iostream>
-#include <DGtal/io/readers/PGMReader.h>
-#include "DGtal/images/imagesSetsUtils/SetFromImage.h"
-#include "DGtal/images/ImageContainerBySTLVector.h"
-#include "../Tools/LireImgPgm.h"
-///////////////////////////////////////////////////////////////////////////////
+#include "Traitement_AntiBruit.h"
 
 using namespace std;
 using namespace DGtal;
---
+++ src/Modifieurs/Traitement_AntiBruit.h
@@ -0,0 +1,46 @@
+#ifndef TRAITEMENT_ANTIBRUIT_H
+#define TRAITEMENT_ANTIBRUIT_H
+
+///////////////////////////////////////////////////////////////////////////////
+#include <iostream>
+#include <vector>
+#include <DGtal/base/Common.h>
+#include <DGtal/io/boards/Board2D.h>
+#include <DGtal/helpers/StdDefs.h>
+#include <DGtal/geometry/curves/ArithmeticalDSSComputer.h>
+#include <iostream>
+#include <DGtal/io/readers/PGMReader.h>
+#include "DGtal/images/imagesSetsUtils/SetFromImage.h"
+#include "DGtal/images/ImageContainerBySTLVector.h"
+#include "../Tools/LireImgPgm.h"
+///////////////////////////////////////////////////////////////////////////////
+
+			
+/* Fait une liste des huits voisins d'un point */
+std::vector<DGtal::Z2i::Point> Make_Voisins(DGtal::Z2i::Point p);
+
+Image Apply_AntiBruit (Image image);
+
+Image Traitement_AntiBruit (std::string filename);
+
+    
+#endif    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+    
+
+    
+
+   
