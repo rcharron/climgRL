0be128d063479ecd3ff6e7f358c99be4d6badef5
rcharron <rcphael.charrondiere@ens-lyon.fr>
2015-04-26 17:17:07 +0200

    Revert "chgmt a abandonner"
    This reverts commit 2d831022f832c0f557d250a52efad3ff4ebe7a77.

--- src/Tests/Remplissage/Test_Remplissage.cpp
+++ src/Tests/Remplissage/Test_Remplissage.cpp
@@ -6,12 +6,11 @@ using namespace DGtal::Z2i; //We'll only consider Z² digital space on
 			    //32bit integers
 
 void Test_Remplissage (){
-  Image test1 = PGMReader<Image>::importPGM("../database/butterfly-10.pgm");
+  MetaImage test1("../database/butterfly-10.pgm");
   Remplissage(test1);
-//  PGMWriter<Image>::exportPGM("../src/Tests/Remplissage/butterfly-10-rempli.pgm",test1);
+  test1.saveSVG("../src/Tests/Remplissage/butterfly-10-rempli.svg");
   //static_cast<Board2D>(test1).saveEPS("rat-8-inverse.eps");
 
-  /*
   MetaImage test2("../database/device9-6.pgm");
   Remplissage(test2);
   test2.saveSVG("../src/Tests/Remplissage/device9-6-rempli.svg");
@@ -27,6 +26,5 @@ void Test_Remplissage (){
 
   MetaImage test5("../database/cup-12.pgm");
   Remplissage(test5);
-  test5.saveSVG("../src/Tests/Remplissage/cup-12-rempli.svg");
-  */
+  test5.saveSVG("../src/Tests/Remplissage/cup-12-rempli.svg");    
 }
