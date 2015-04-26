fe5ff23f1919c68d8024a09832defe92a6915344
laureline@laureline <laureline.pinault@ens-lyon.fr>
2015-04-26 16:00:58 +0200

    Header de remplissage

--- src/Modifieurs/Remplissage.cpp
+++ src/Modifieurs/Remplissage.cpp
@@ -1,17 +1,4 @@
-///////////////////////////////////////////////////////////////////////////////
-#include <iostream>
-#include <vector>
-#include <queue>
-#include <DGtal/base/Common.h>
-#include <DGtal/io/boards/Board2D.h>
-#include <DGtal/helpers/StdDefs.h>
-#include <DGtal/geometry/curves/ArithmeticalDSSComputer.h>
-#include <iostream>
-#include <DGtal/io/readers/PGMReader.h>
-#include "DGtal/images/imagesSetsUtils/SetFromImage.h"
-#include "DGtal/images/ImageContainerBySTLVector.h"
-#include "../MetaImage.h"
-///////////////////////////////////////////////////////////////////////////////
+#include "Remplissage.h"
 
 /*
 REMPLISSAGE
