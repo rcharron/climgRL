a4e0cfb6d76f5eff39ee4a34fa4a7aaa133e4344
laureline@laureline <laureline.pinault@ens-lyon.fr>
2015-05-03 23:23:14 +0200

    export des données vers graphe

--- Rapport/Graphes/Affichage_graph_basicestim.py
+++ Rapport/Graphes/Affichage_graph_basicestim.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import matplotlib
 
 #y = np.transpose(np.loadtxt('models-solidestim.txt',unpack=True,dtype=['str','str',float,float]))
-y = np.loadtxt('models-spineestim.txt', dtype='str')
+y = np.loadtxt('models-basicestim.txt', dtype='str')
 
 y = np.transpose(y)
 
@@ -12,7 +12,7 @@ y = np.transpose(y)
 liste = np.zeros(np.shape(y))
 
 
-for i in range(2,4):
+for i in range(2,6):
     for k in range(np.shape(y)[1]):
         liste[i][k] = float(y[i][k])
 
@@ -20,7 +20,10 @@ for i in range(2,4):
 
 
 for i in range(0, len(liste[2])):
-    plt.errorbar(i,liste[2][i], yerr=liste[3][i], fmt = 'ro')
+    plt.errorbar(i,liste[2][i], yerr=liste[4][i], fmt = 'ro')
+    
+for i in range(0, len(liste[2])):
+    plt.errorbar(i,liste[3][i], yerr=liste[5][i], fmt = 'ro')
 
 matplotlib.rc('xtick', labelsize=20)
 matplotlib.rc('ytick', labelsize=40)
@@ -28,7 +31,7 @@ matplotlib.rc('ytick', labelsize=40)
 x = range(0,len(liste[2]))
 plt.xticks(x, y[1], rotation = 60)
 #plt.plot(liste[3], 'ro')
-plt.ylabel("Spine", fontsize=16)
+plt.ylabel("Basicestim", fontsize=16)
 
 plt.show()
 
